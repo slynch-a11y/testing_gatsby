4c297a6ea1a911f20aa86f82ab0bc037bc6ba0b3
Lynch <sarah.lynch@yale.edu>
2019-04-20 17:13:31 -0400

    Updates

---
+++ styles-aad0ab7aeeafa639a0b4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{150:function(s,e,o){s.exports={user:"about-css-modules-module--user--1r5hF",avatar:"about-css-modules-module--avatar--1-xsJ",description:"about-css-modules-module--description--3EV0A",username:"about-css-modules-module--username--1Craw",excerpt:"about-css-modules-module--excerpt--2hQyc"}}}]);
+//# sourceMappingURL=styles-aad0ab7aeeafa639a0b4.js.map
